c5747353baa4012605903464334629e75b432624
lucas <0000600853@senaimgaluno.com.br>
2023-08-23 21:41:22 -0300

    Estou subindo o siste pronto e editado professor

--- admin/Shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('price/', views.price, name='produto'),
-    path('cart/', views.cart, name='carrinho'),
-]
